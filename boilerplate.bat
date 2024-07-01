842e3a30eef375512b2f084327f6bbd51c4d0224
keshavsoft <nknnkumar@yahoo.com>
2024-07-01 16:42:11 +0530

    keshav

--- boilerplate.bat
+++ boilerplate.bat
@@ -1,5 +1,3 @@
-call if not exist "src\\FrontEnd\\AllTables"
-call mkdir "src/FrontEnd/AllTables"
 call npm i
 call node KCode/Backend.js
 call node KCode/Database.js
--- fbd.bat
+++ fbd.bat
@@ -1,6 +1,3 @@
-call if not exist "src\\FrontEnd\\AllTables"
-call mkdir "src/FrontEnd/AllTables"
 call node KCode/Backend.js
 call node KCode/Database.js
 call npm run build
-npm run start
