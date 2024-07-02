23b243fb7aa1044b0f65a7e49ae78415b9a68d94
Srivasu215 <sri.vasu215@gmail.com>
2024-07-02 15:35:39 +0530

    SREENIVAS

--- Common/CommonBin/FileNames/AsCollection/repos/getFuncs/EntryFile.js
+++ Common/CommonBin/FileNames/AsCollection/repos/getFuncs/EntryFile.js
@@ -1,6 +1,11 @@
 import { GetFunc as GetFuncDal } from '../../dals/getFuncs/EntryFile.js';
+import { GetFunc as dalsForSequelize } from '../../dalsForSequelize/getFuncs/EntryFile.js';
+import ConfigJson from '../../../../../../bin/Config.json' assert {type: 'json'};
 
-let GetFunc = () => {
+let GetFunc = async () => {
+    if (ConfigJson.isSequelize) {
+        return await dalsForSequelize();
+    };
     return GetFuncDal();
 };
 
