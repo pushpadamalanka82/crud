31732483a657e088c3c04068d748f1ceb246ea39
Srivasu215 <sri.vasu215@gmail.com>
2024-07-04 20:00:58 +0530

    SREENIVAS

--- src/FrontEnd/assets/static/js/pages/DeleteSSR/FormLoad/BuildBsTable/EntryFile.js
+++ src/FrontEnd/assets/static/js/pages/DeleteSSR/FormLoad/BuildBsTable/EntryFile.js
@@ -1,5 +1,5 @@
 import { StartFunc as StartFuncTableTag } from "./TableTag.js";
-import { StartFunc as StartFuncForColumns } from "./ForColumns/EntryFile.js";
+// import { StartFunc as StartFuncForColumns } from "./ForColumns/EntryFile.js";
 import { StartFunc as onClickRow } from "./onClickRow/EntryFile.js";
 
 const StartFunc = () => {
@@ -9,7 +9,7 @@ const StartFunc = () => {
 
 const jFLocalInitialize = async () => {
     var $table = $('#table');
-    let jVarLocalData = await StartFuncForColumns();
+    // let jVarLocalData = await StartFuncForColumns();
 
     $table.bootstrapTable({
         data: [],
