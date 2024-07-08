35630eaaaf0aa450a01ab414732119447402f5d2
keshavsoft <nknnkumar@yahoo.com>
2024-07-08 18:32:17 +0530

    keshav

--- src/FrontEnd/assets/static/js/pages/CreateSSR/FormLoad/AddListeners/SaveButtonId/FetchFromFuncs/AfterFetch/EntryFile.js
+++ src/FrontEnd/assets/static/js/pages/CreateSSR/FormLoad/AddListeners/SaveButtonId/FetchFromFuncs/AfterFetch/EntryFile.js
@@ -1,13 +1,15 @@
 import UrlJson from './url.json' with {type: 'json'};
 
 let StartFunc = () => {
-    LocalFuncForSingleTable();
-    LocalFuncForAllTables();
+    if (LocalFuncForSingleTable() === false) {
+        LocalFuncForAllTables();
+    };
 };
 
 const LocalFuncForSingleTable = () => {
-    if (window.location.pathname.endsWith(`//${UrlJson.PresentUrl}`)) {
+    if (window.location.pathname.endsWith(`/${UrlJson.PresentUrl}`)) {
         window.location.href = `${UrlJson.RedirectToUrl}`;
+        return true;
     };
 };
 
