7a1f4e66e8ad2e8f4b7df3b91983b68cd7045de5
keshavsoft <nknnkumar@yahoo.com>
2024-07-09 16:40:24 +0530

    keshav

--- src/FrontEnd/assets/static/js/pages/AlterWithSSR/FormLoad/BuildBsTable/onClickRow/AfterFetch.js
+++ src/FrontEnd/assets/static/js/pages/AlterWithSSR/FormLoad/BuildBsTable/onClickRow/AfterFetch.js
@@ -2,8 +2,28 @@ import UrlJson from './url.json' with {type: 'json'};
 
 let StartFunc = ({ inRowPk }) => {
     let LocalRowPk = inRowPk;
+    console.log("LocalRowPk : ", LocalRowPk);
+    if (LocalFuncForSingleTable({ inRowPk }) === false) {
+        LocalFuncForAllTables({ inRowPk });
+    };
+
+    // window.location.href = `${jVarGlobalTableName}${UrlJson.RedirectToUrl}?inRowPk=${LocalRowPk}`;
+};
+
+const LocalFuncForSingleTable = ({ inRowPk }) => {
+    let LocalRowPk = inRowPk;
+
+    if (window.location.pathname.endsWith(`/${UrlJson.PresentUrl}`)) {
+        window.location.href = `${UrlJson.RedirectToUrl}?inRowPk=${LocalRowPk}`;
+        return true;
+    };
+};
+
+const LocalFuncForAllTables = ({ inRowPk }) => {
+    let LocalRowPk = inRowPk;
 
     window.location.href = `${jVarGlobalTableName}${UrlJson.RedirectToUrl}?inRowPk=${LocalRowPk}`;
 };
+
 
 export { StartFunc }
